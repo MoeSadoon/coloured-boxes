d6a1af4d74ded9df2a418ce2de0a56503d5c9022
Moe Sadoon <msadoon590@gmail.com>
2017-12-24 20:24:33 +0000

    refactored

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
-const Box = props => (
-    <li className="box_item" style={{backgroundColor: props.color}}> . </li>
-)
+const NUM_BOXES = 32;
 
-const Grid = props => {
-  const boxes = props.colors.map((item, idx) => {
-    return(
-        <Box color = {item}/>
-    )
-  });
-  return (
-    <ul className="box_container">
-      {boxes}
-    </ul>
-  )
-}
-
+// App stateful component - this sets the state
 class App extends Component {
   constructor(props) {
     super(props);
+    const displayColors = Array(NUM_BOXES).fill().map(this.getRandomColor.bind(this));
+    this.state = {displayColors}
 
-    this.state = {
-      displayColors: this.props.allColors.sort(() => Math.random() - 0.5).slice(0, 32)
-    }
-
+    // The logic where a random box changes to a random color
     setInterval(() => {
-      // Select random box
-      const randBox = Math.floor(Math.random() * this.state.displayColors.length);
-      // Select random color from allColors array
-      const randColor = Math.floor(Math.random() * this.props.allColors.length);
-      // Replace color of random box with new random color
-      const displayColors = this.state.displayColors.map((color, idx, newArr) => {
+      const randBox = Math.floor(Math.random() * this.state.displayColors.length); // Selects random box    
+      const displayColors = this.state.displayColors.map((color, idx) => {
        if(idx === randBox) {
-        color = this.props.allColors[randColor];
+        color = this.getRandomColor();
         return color;
        }
        return color
       });
       this.setState({displayColors});
     }, 300)
+  };
+
+  static defaultProps = {
+    allColors: ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond",
+    "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate",
+    "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod",
+    "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange",
+    "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey",
+    "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue",
+    "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod",
+    "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki",
+    "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan",
+    "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon",
+    "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow",
+    "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid",
+    "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise",
+    "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy",
+    "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen",
+    "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue",
+    "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen",
+    "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen",
+    "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke",
+    "Yellow", "YellowGreen"],
+  }
+
+  getRandomColor() {
+    let randColorIdx = Math.floor(Math.random() * this.props.allColors.length);
+    return this.props.allColors[randColorIdx];
   }
  
   render() {
@@ -50,31 +60,35 @@ class App extends Component {
   }
 }
 
-App.defaultProps = {
-  allColors: ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond",
-  "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate",
-  "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod",
-  "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange",
-  "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey",
-  "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue",
-  "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod",
-  "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki",
-  "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan",
-  "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon",
-  "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow",
-  "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid",
-  "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise",
-  "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy",
-  "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen",
-  "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue",
-  "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen",
-  "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen",
-  "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke",
-  "Yellow", "YellowGreen"],
+// Grid stateless functional component, responsible for rendering the boxes
+const Grid = props => {
+  const boxes = props.colors.map((item, idx) => (
+    <Box key={idx} color ={item}/>
+  ));
+  return (
+    <ul className="box_container">
+      {boxes}
+    </ul>
+  );
+}
+
+Grid.propTypes = {
+  colors: PropTypes.arrayOf(PropTypes.string)
 }
 
 
 
+// Box stateless functional component, responsible for displaying its color
+const Box = props => {
+  const style = {backgroundColor: props.color};
+  return (
+    <li key={props.key} className="box_item" style={style}>.</li>
+  )
+}
+
+Box.propTypes = {
+  color: PropTypes.string
+}
 
 export default App;
 
